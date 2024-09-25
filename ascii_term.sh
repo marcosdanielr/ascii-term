53f9692d9385f9b9cb6d7b4fbe46842aab986413
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-09-24 23:57:22 -0300

    feat: custom color

--- ascii_term.sh
+++ ascii_term.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 ASCII_FILE="$HOME/.config/ascii-term/ascii.txt"
+COLOR_FILE="$HOME/.config/ascii-term/color.txt"
+
+RESET_COLOR="\033"
 
 if [[ ! -f "$ASCII_FILE" ]]; then
     echo -e "\033[31mError: '$ASCII_FILE' not found"
+    exit 1
+fi
+
+if [[ -f "$COLOR_FILE" ]]; then
+    COLOR=$(cat "$COLOR_FILE")
+    echo -e "${COLOR}"
+    cat "$ASCII_FILE"
+    echo -e "${RESET_COLOR}"
     exit 1
 fi
 
